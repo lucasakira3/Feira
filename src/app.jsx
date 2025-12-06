8ae7bebeb11c9b7406a0db8ea4cad769bd3cd41a
Akira Teruya <56326698+lucasakira3@users.noreply.github.com>
2025-12-06 14:14:08 -0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -10,7 +10,7 @@ import Produtos from "./Pages/Produtos.jsx";
 import Login from "./Pages/Login.jsx";
 import Index from "./Pages/Index.jsx";
 import Sobrenos from "./Pages/sobrenos.jsx";
-import Cadastro from "./Pages/Cadastro.jsx";
+import Cadastro from "./Pages/cadastro.jsx";
 import Faleconosco from "./Pages/FaleConosco.jsx";
 import Entrar from "./Pages/Entrar.jsx";
 import Carrinho from "./Pages/Carrinho.jsx";
